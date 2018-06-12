94421e3720a2bd18d8857d9011ef690d201670e6
David Rose <daviderose526@gmail.com>
2018-06-11 19:48:10 -0500

    Create diagonalDifference.js

---
+++ diagonalDifference.js
@@ -0,0 +1,53 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    var a = [];
+    for(a_i = 0; a_i < n; a_i++){
+       a[a_i] = readLine().split(' ');
+       a[a_i] = a[a_i].map(Number);
+    }
+
+}
+
+function processData(input) {
+    var lines = input.split("\n");
+    var N = parseInt(lines[0]);
+    var ans = 0;
+    for(i = 1; i <= N; i++) {
+        var line = lines[i].split(" ");
+        ans += parseInt(line[i - 1]) - parseInt(line[N - i]);
+    }
+    console.log(Math.abs(ans));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
